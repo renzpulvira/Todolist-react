faa554863c1a891cf00c0bfb6025ef5f1aae0e9f
renzpulvira <calypsogaming098two@gmail.com>
2019-08-29 03:52:02 +0800

    Updates

---
+++ precache-manifest.f8526689f4b8839ed31a3c2928821964.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6a6eae4cd14ae1e02fe57aff65982ba",
+    "url": "/Todolist-react/index.html"
+  },
+  {
+    "revision": "29d2b8dbc82a7f61d10e",
+    "url": "/Todolist-react/static/css/main.63b03d8d.chunk.css"
+  },
+  {
+    "revision": "c6711347337c0bdbd6ef",
+    "url": "/Todolist-react/static/js/2.c0c080c6.chunk.js"
+  },
+  {
+    "revision": "29d2b8dbc82a7f61d10e",
+    "url": "/Todolist-react/static/js/main.e8a64899.chunk.js"
+  },
+  {
+    "revision": "7c6b9a56b8a7b6df3999",
+    "url": "/Todolist-react/static/js/runtime~main.f27f5f04.js"
+  }
+]);
